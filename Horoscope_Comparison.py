8f99a3a9c4c3ef247628134546ca9fa1b7bbc908
FXelix <felix.scheling@web.de>
2017-06-29 16:13:04 +0200

    Initial commit

---
+++ Horoscope_Comparison.py
@@ -0,0 +1,12 @@
+
+"""
+This script is about learning requests and BeautifulSoup to scrape websites.
+This script is about your daily horoscope from 3 different websites:
+1. https://www.astrology.com/
+2. https://www.horoscope.com/us/index.aspx
+3. http://astrostyle.com/daily-horoscopes/
+
+I dont't believe in astrology nor do I affiliate with those website. This scirpt just compares their
+statements about your specific daily horoscope.
+"""
+
